6324be1c275cd7961a0c735b77d3166557275fb5
Frampolz <francesco.colloca@hotmail.it>
2022-02-02 16:16:25 +0100

    add v-for

--- index-ajax.php
+++ index-ajax.php
@@ -10,7 +10,21 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <div id="app">
+         <div class="album-container">
+            <div v-for="(album, index) in albums" class="album-box">
+                <div class="album-cover">
+                    <img :src="album.poster" :alt="album.title" />
+                </div>
+                <div class="album-details">
+                    <h2> {{album.title}} </h2>
+                    <h3>{{ album.author }}</h3>
+                    <h4>{{ album.year }}</h4>
+                </div>
+            </div>
+        </div> 
+        
+    </div> 
 <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 <script src="js/script.js"></script>
 </body>
