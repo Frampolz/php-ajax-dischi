9334ccbe51410687187c05eb609c76e98446c63b
Frampolz <francesco.colloca@hotmail.it>
2022-02-02 16:37:43 +0100

    fix function

--- js/script.js
+++ js/script.js
@@ -1,17 +1,15 @@
 const app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!',
     albums: [],
   },
   created() {
     //chiamata axios del file json
     axios.get('http://localhost/php-ajax-dischi/server/controller-api.php')
-  .then(function (response) {
-    // handle success
-   this.albums = response.data.results;
-   console.log(this.albums);
-  })
+  .then((response => {
+    this.albums= response.data.results
+    console.log(this.albums);
+  }))
   .catch(function (error) {
     // handle error
     console.log(error);
