1b91b3c9dd73fceb8c460476edae590dc52ea7f4
Frampolz <francesco.colloca@hotmail.it>
2022-02-02 16:16:16 +0100

    add axios call

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 const app = new Vue({
   el: '#app',
   data: {
-    
+    message: 'Hello Vue!',
+    albums: [],
+  },
+  created() {
+    //chiamata axios del file json
+    axios.get('http://localhost/php-ajax-dischi/server/controller-api.php')
+  .then(function (response) {
+    // handle success
+   this.albums = response.data.results;
+   console.log(this.albums);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  });
   }
 });
